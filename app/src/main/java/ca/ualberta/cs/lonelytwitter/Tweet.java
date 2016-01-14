84244d1c30a7d83524ef9289a61fb0d1bdc45aab
austinredmond <aredmond@ualberta.ca>
2016-01-14 16:36:04 -0700

    Updated Tweet.java

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,6 +1,8 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by aredmond on 1/14/16.
@@ -8,15 +10,18 @@ import java.util.Date;
 public abstract class Tweet {
     protected String message;
     private Date date;
+    protected ArrayList<Mood> moods;
 
     public Tweet(String message, Date date) {
         this.message = message;
         this.date = date;
+        this.moods = new ArrayList();
     }
 
     public Tweet(String message){
         this.message = message;
         this.date = new Date(System.currentTimeMillis());
+        this.moods = new ArrayList();
     }
 
     public Date getDate() {
@@ -33,6 +38,14 @@ public abstract class Tweet {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public void addMood(Mood mood){
+        moods.add(mood);
+    }
+
+    public List getMoods() {
+        return moods;
     }
 
     abstract public boolean isImportant();
